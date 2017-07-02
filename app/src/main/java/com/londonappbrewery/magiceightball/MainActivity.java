245020a5d7e5aca277007482f94eb5ea600305b9
robertgolusinski <robertgolusinski@gmail.com>
2017-07-02 20:14:04 +0200

    Modified the app to change the picture when user shakes the phone.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,8 +1,14 @@
 package com.londonappbrewery.magiceightball;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,13 +16,22 @@ import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
     int previousNumber = 0;
     Random randomNumberGenerator = new Random();
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
 
         final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
 
@@ -29,18 +44,30 @@ public class MainActivity extends AppCompatActivity {
                         R.drawable.ball5,
                 };
 
-        Button myButton = (Button) findViewById(R.id.askButton);
-
-        myButton.setOnClickListener(new View.OnClickListener()
+        mShakeDetector.setOnShakelistener(new ShakeDetector.OnShakeListener()
         {
             @Override
-            public void onClick(View v)
+            public void onShake(int count)
             {
                 previousNumber = differentNumber();
 
                 ballDisplay.setImageResource(ballArray[previousNumber]);
             }
         });
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause()
+    {
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 
     protected int differentNumber()
